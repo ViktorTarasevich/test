072046375607b17405e9442d895f0f14614ad913
PC <vitjaa.tarasevich@gmail.com>
2022-09-29 13:01:02 +0300

    fix: converting a second file to a new format

--- app.js
+++ app.js
@@ -13,45 +13,37 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 //app.use('/api')
-
-async function parsFile(error, data) {
+async function parsFile(data) {
     try {
-        if (error) throw error;
-        //data = data.trim();
         const parseFile =  JSON.parse(data);
-        for (let i = 0; i < parseFile.length; i++) {
-            //console.log(parseFile[i])
-            //console.log(parseFile[i].offers)
-        }
         return newFormat(parseFile)
     } catch (error) {
         console.log('error', error)
     }
 }
 async function newFormat(parseFile) {
-    //console.log('parseFile----', parseFile)
     try {
         const result = []
         const length = parseFile.length ?? parseFile.RESPONSE.length
         for (let i =  0; i < length; i++){
-            const data =  parseFile[i]?.offers ? parseFile[i].offers[0] : null ?? parseFile.RESPONSE ? parseFile.RESPONSE : null
+            const data =  parseFile[i]?.offers ? parseFile[i].offers[0] : null ?? parseFile.RESPONSE ? parseFile.RESPONSE[i] : null
                 ?? parseFile[i]
             const form = {
-                provider: data.provider ?? data.PROVEDER ??  null,
+                provider: data.provider || data.PROVEDER ||  null,
                 name: data.name || data.NAME || data.article?.name || null,
-                brand: data.brand ?? data.BRAND ?? data.article?.tradeMarkName,
-                in_stock: data.in_stock ?? data.IN_STOCK ?? data.prices?.length > 0 ? data.prices[0].quantity.available : null,
+                brand: data.brand || data.BRAND || data.article?.tradeMarkName,
+                in_stock: data.in_stock || data.IN_STOCK ||  data.prices?.at(0, 0).quantity.available || null,
                 price: {
-                    type: data.price ?? data.PRICE ?? data.prices?.length > 0 ? data.prices[0].price.value : null
+                    type: data.price || data.PRICE || data.prices?.at(0, 0).price.value
                 },
-                additional_info: data.additional_info ?? data.ADDITIONAL_INFO ?? data.prices?.length > 0 ? data.prices[0].addInfo : null,
+                additional_info: data.additional_info || data.ADDITIONAL_INFO ||  data.prices?.at(0, 0).addInfo || null,
                 articul: {
-                    type: data.articul || data.ARTICUL || data.prices?.length > 0 ? data.prices[0].articleId : null,
+                    type: data.articul || data.ARTICUL || data.prices?.at(0, 0).articleId || null,
                 }
             }
+            console.log(form)
             result.push(form)
         }
-        console.log('result', result)
     } catch (error) {
         console.log('error', error)
     }
@@ -59,21 +51,24 @@ async function newFormat(parseFile) {
 
 fs.readFile('file/4.txt', 'utf8',  async (error, data) => {
      try {
-         const nameFile4 =  parsFile(error, data)
+         if (error) throw error;
+         await parsFile(data)
      } catch (err4) {
          console.log('err4', err4)
      }
 })
 fs.readFile('file/first.txt', 'utf8',  async (error, data) => {
      try {
-         const nameFileFirst =  parsFile(error, data)
+         if (error) throw error;
+         await parsFile(data)
      } catch (errFirst) {
          console.log('errFirst', errFirst)
      }
 })
 fs.readFile('file/second.txt', 'utf8',  async (error, data) => {
      try {
-         const nameFileSecond =  parsFile(error, data)
+         if (error) throw error;
+          await parsFile(data)
      } catch (errSecond) {
          console.log('errSecond', errSecond)
      }
